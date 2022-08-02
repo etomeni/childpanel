b8ec3cd2d47e619c46d0ac2d159bbccf70f1520e
Sunday Etom Eni <sundaywht@gmail.com>
2022-08-02 11:34:27 +0100

    exported sql

--- backend/middleware/orderCheck.js
+++ backend/middleware/orderCheck.js
@@ -26,51 +26,51 @@ export const checkUsersPendingOrders = async (req, res, next) => {
 
     if (orders.length > 0) {
 
-        // for (const order of orders) {
-        //     if (order.providerOrderID || order.providerOrderID != null) {
-        //         let apiText = `${apiProvider.url}?key=${apiProvider.apiKey}&action=status&order=${order.providerOrderID}`;
-        //         const orderIdRes = await axios.post(apiText);
+        for (const order of orders) {
+            if (order.providerOrderID || order.providerOrderID != null) {
+                let apiText = `${apiProvider.url}?key=${apiProvider.apiKey}&action=status&order=${order.providerOrderID}`;
+                const orderIdRes = await axios.post(apiText);
 
-        //         if (orderIdRes.data.status == "Refunded") {
-        //             // get the user current data to check bal
-        //             let Cuser = await user.getCurrentUser({userID: getOrderData.userID});
-        //             Cuser = Cuser[0][0];
+                if (orderIdRes.data.status == "Refunded") {
+                    // get the user current data to check bal
+                    let Cuser = await user.getCurrentUser({userID: getOrderData.userID});
+                    Cuser = Cuser[0][0];
 
-        //             const RefundBal = {
-        //                 colombName: ["balance"],
-        //                 NewColombNameValue: [Cuser.balance + order.amount],
+                    const RefundBal = {
+                        colombName: ["balance"],
+                        NewColombNameValue: [Cuser.balance + order.amount],
                 
-        //                 conditionColombName: ["id"],
-        //                 conditionColombValue: [Cuser.id]
-        //             };
+                        conditionColombName: ["id"],
+                        conditionColombValue: [Cuser.id]
+                    };
                     
-        //             await auth.updateUser(RefundBal);
-        //         }
+                    await auth.updateUser(RefundBal);
+                }
 
-        //         const updateData = {
-        //             colombName: ["apiCharge", "status", "startCount", "remains"],
-        //             NewColombNameValue: [`${orderIdRes.data.charge}`, `${orderIdRes.data.status}`, `${orderIdRes.data.start_count}`, `${orderIdRes.data.remains}`],
+                const updateData = {
+                    colombName: ["apiCharge", "status", "startCount", "remains"],
+                    NewColombNameValue: [`${orderIdRes.data.charge}`, `${orderIdRes.data.status}`, `${orderIdRes.data.start_count}`, `${orderIdRes.data.remains}`],
         
-        //             conditionColombName: ["id"],
-        //             conditionColombValue: [`${order.id}`]
-        //         };
-        //         await general.updateOrder(updateData, "AND");
-        //     } else {
-        //         let apiText = `${apiProvider.url}?key=${apiProvider.apiKey}&action=add&service=${order.serviceID}&link=${order.link}&quantity=${order.quantity}`;
-        //         const response = await axios.post(apiText);
+                    conditionColombName: ["id"],
+                    conditionColombValue: [`${order.id}`]
+                };
+                await general.updateOrder(updateData, "AND");
+            } else {
+                let apiText = `${apiProvider.url}?key=${apiProvider.apiKey}&action=add&service=${order.serviceID}&link=${order.link}&quantity=${order.quantity}`;
+                const response = await axios.post(apiText);
         
-        //         if (response.data.order) {
-        //             let data = {
-        //                 colombName: ["providerOrderID"],
-        //                 NewColombNameValue: [`${response.data.order}`],
+                if (response.data.order) {
+                    let data = {
+                        colombName: ["providerOrderID"],
+                        NewColombNameValue: [`${response.data.order}`],
             
-        //                 conditionColombName: ["id"],
-        //                 conditionColombValue: [`${order.id}`]
-        //             };
-        //             await general.updateOrder(data, "AND");
-        //         }
-        //     }
-        // }
+                        conditionColombName: ["id"],
+                        conditionColombValue: [`${order.id}`]
+                    };
+                    await general.updateOrder(data, "AND");
+                }
+            }
+        }
     }
 
 }
